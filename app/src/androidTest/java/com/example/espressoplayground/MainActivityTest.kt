a05021550638c458172452fc0a5945d4529c201b
Alessandro <aleangelozi@gmail.com>
2022-11-20 20:16:29 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/espressoplayground/MainActivityTest.kt
@@ -0,0 +1,73 @@
+package com.example.espressoplayground
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.milesDisplay),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.milesDisplayLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("04935689994"), closeSoftKeyboard())
+
+        val editText = onView(
+            allOf(
+                withId(R.id.milesDisplay), withText("04935689994"),
+                withParent(withParent(withId(R.id.milesDisplayLayout))),
+                isDisplayed()
+            )
+        )
+        editText.check(matches(withText("04935689994")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
